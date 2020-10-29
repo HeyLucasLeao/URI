3c80c6c3ce288929b426155ad1f4c5cbcaf8e72c
Lucas Leão <ilolt.llol@gmail.com>
2020-10-29 08:23:34 -0400

    Add files via upload
    https://www.urionlinejudge.com.br/judge/pt/problems/view/2502

---
+++ 2502.py
@@ -0,0 +1,46 @@
+import re
+
+ent = (int(x) for x in input().split())
+x,y = ent
+tradu = input()
+cripto = input()
+txt = ""
+
+for _ in range(y):
+    txt += input() + '\n'
+
+iter_cripto = tuple((x,y) for x,y in zip(cripto,tradu))
+
+def convert_to_binary_str(inp: str) -> str:
+    return (''.join(format(ord(x), 'b') for x in inp))
+
+
+def regex_to_lower(txt: str) -> str:
+    res = txt
+    for cripto,tradu in iter_cripto:
+        res = re.sub(f"[{cripto.lower()}]+",f"_{convert_to_binary_str(tradu.lower())}_",res)
+
+    for cripto,tradu in iter_cripto:
+        res = re.sub(f"[{tradu.lower()}]+", cripto.lower(),res)
+
+    for cripto,tradu in iter_cripto:
+        res = re.sub(f"_{convert_to_binary_str(tradu.lower())}_", tradu.lower(),res)
+    return res
+    
+def regex_to_upper(res: str) -> str:
+    for cripto,tradu in iter_cripto:
+        res = re.sub(f"[{cripto.upper()}]+",f"_{convert_to_binary_str(tradu.upper())}_",res)
+
+    for cripto,tradu in iter_cripto:
+        res = re.sub(f"[{tradu.upper()}]+", cripto.upper(),res)
+
+    for cripto,tradu in iter_cripto:
+        res = re.sub(f"_{convert_to_binary_str(tradu.upper())}_", tradu.upper(),res)
+    return res
+
+def descripto(inp: str) -> str:
+    return regex_to_upper(regex_to_lower(inp))
+
+
+
+print(descripto(txt))
