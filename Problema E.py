83b2102105ccf7de9f69de5bf7a7c174b2f1f967
Lucas Leão <ilolt.llol@gmail.com>
2020-12-22 21:40:31 -0400

    Create Problema E.py

---
+++ Problema E.py
@@ -0,0 +1,14 @@
+qtd, efetuados = [int(x) for x in input().split()]
+inp = [[int(x) for x in input().split()] for _ in range(efetuados)]
+inp = [sorted(x) for x in inp]
+inp.sort()
+etiquetas = set(range(1, qtd + 1))
+ligacoes = set(inp[0])
+for i in range(1, len(inp)):
+    if any(x for x in ligacoes if x in inp[i]):
+        ligacoes.add(inp[i][0])
+        ligacoes.add(inp[i][1])
+if ligacoes == etiquetas:
+    print('COMPLETO')
+else:
+    print('INCOMPLETO')
